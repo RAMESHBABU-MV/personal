4c36a8351c47283420f39037249dd790e67d41d8
codemogger-max <rameshbabu10743@gmail.com>
2024-08-08 10:16:40 +0530

    m

---
+++ RSMART/average.py
@@ -0,0 +1,3 @@
+number_1,number2,NUMBER3 = int(input("ENTER NUMBER:")),int(input("ENTER NUMBER:")),int(input("ENTER NUMBER:"))
+average = (number_1+number2+NUMBER3)/3
+print("THE AVERAGE OF THE THREE NUMBER IS: ",average)
